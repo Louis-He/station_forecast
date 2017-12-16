89341fb7ff07f959dca4377cb990c08f25dfe44a
Louis-He <1726110778@qq.com>
2017-12-16 14:16:09 -0500

    update

--- website/webfunc.py
+++ website/webfunc.py
@@ -1,14 +1,34 @@
 import web
 urls = (
     '/index', 'index',
+    '/login', 'login',
     '/setcookie', 'CookieSet',
     '/getcookie', 'CookieGet'
 )
 app = web.application(urls, globals())
 
+allowed = (
+	('admin','admin'),
+)
+
 class index:
     def GET(self):
         return "index Page."
+
+class login:
+    def GET(self):
+        return open(r'login.html', 'r').read()
+
+    def POST(self):
+        i = web.input()
+        username = i.get('username')
+        passwd = i.get('passwd')
+        if (username, passwd) in allowed:
+            i = web.input(access='True')
+            web.setcookie('access', i.access, 5)
+            return 'Login Success'
+        else:
+            return '<h1>Login Error!!!</h1></br><a href="/login">Login</a>'
 
 class CookieSet:
     def GET(self):
