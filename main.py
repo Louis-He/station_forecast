3d307702ca8ca916a03078cd307028abdcda1360
Louis-He <1726110778@qq.com>
2017-12-16 00:06:20 -0500

    update project

--- main.py
+++ main.py
@@ -3,13 +3,23 @@
 #EC
 #https://node.windy.com/forecast/v2.1/ecmwf/43.663/-79.399?setup=summary&includeNow=true&source=hp
 import matplotlib
+import datetime
+import dateutil
 matplotlib.use('Agg')
 
 import json
 import urllib.request
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FuncFormatter
+from matplotlib import ticker, dates
 import time
+
+matplotlib.rcParams['xtick.direction'] = 'out'
+matplotlib.rcParams['ytick.direction'] = 'out'
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def getData(org,lon,lat):
@@ -35,6 +45,15 @@ def getData(org,lon,lat):
         day1dayweather.append(int(data[i]['detail'][0]['day']['weather']['img']))
         day2dayweather.append(int(data[i]['detail'][1]['day']['weather']['img']))
     '''
+    return data
+
+def getdetail(org, lon,lat):
+    if org == 'GFS':
+        data = urllib.request.urlopen('https://node.windy.com/forecast/v2.1/gfs/' + str(lat) +'/' + str(lon) + '?source=detail').read()
+    if org == 'EC':
+        data = urllib.request.urlopen('https://node.windy.com/forecast/meteogram/ecmwf/' + str(lat) +'/' + str(lon)).read()
+    record = data.decode('UTF-8')
+    data = json.loads(record)
     return data
 
 def analyze(source,JSON):
@@ -76,6 +95,153 @@ def analyze(source,JSON):
 
     graph(source,TS,T,pp)
 
+def analyzedetailT(source,JSON):
+    model = JSON['header']['model']
+    reftime = JSON['header']['refTime']
+    T1000 = JSON['data']['temp-1000h']
+    T950 = JSON['data']['temp-950h']
+    T900 = JSON['data']['temp-900h']
+    T850 = JSON['data']['temp-850h']
+    T800 = JSON['data']['temp-800h']
+    T700 = JSON['data']['temp-700h']
+    T600 = JSON['data']['temp-600h']
+    T500 = JSON['data']['temp-500h']
+    T400 = JSON['data']['temp-400h']
+    T300 = JSON['data']['temp-300h']
+    T200 = JSON['data']['temp-200h']
+
+    T750 = []
+    T650 = []
+    T550 = []
+    T450 = []
+    T350 = []
+    T250 = []
+    count = 0
+    for i in T800:
+        T1000[count] -= 273.15
+        T950[count] -= 273.15
+        T900[count] -= 273.15
+        T850[count] -= 273.15
+        T800[count] -= 273.15
+        T700[count] -= 273.15
+        T600[count] -= 273.15
+        T500[count] -= 273.15
+        T400[count] -= 273.15
+        T300[count] -= 273.15
+        T200[count] -= 273.15
+        T750.append((T700[count]+T800[count])/2.0)
+        T650.append((T600[count] + T700[count]) / 2.0)
+        T550.append((T500[count] + T600[count]) / 2.0)
+        T450.append((T400[count] + T500[count]) / 2.0)
+        T350.append((T300[count] + T400[count]) / 2.0)
+        T250.append((T200[count] + T300[count]) / 2.0)
+        count += 1
+
+    print('T analyzed.')
+    return [T1000,T950,T900,T850,T800,T750,T700,T650,T600,T550,T500,T450,T400,T350,T300,T250,T200]
+
+def analyzedetailRH(source,JSON):
+    model = JSON['header']['model']
+    reftime = JSON['header']['refTime']
+    RH1000 = JSON['data']['rh-1000h']
+    RH950 = JSON['data']['rh-950h']
+    RH900 = JSON['data']['rh-900h']
+    RH850 = JSON['data']['rh-850h']
+    RH800 = JSON['data']['rh-800h']
+    RH700 = JSON['data']['rh-700h']
+    RH600 = JSON['data']['rh-600h']
+    RH500 = JSON['data']['rh-500h']
+    RH400 = JSON['data']['rh-400h']
+    RH300 = JSON['data']['rh-300h']
+    RH200 = JSON['data']['rh-200h']
+
+    RH750 = []
+    RH650 = []
+    RH550 = []
+    RH450 = []
+    RH350 = []
+    RH250 = []
+    count = 0
+    for i in RH800:
+        RH750.append((RH700[count] + RH800[count])/2.0)
+        RH650.append((RH600[count] + RH700[count]) / 2.0)
+        RH550.append((RH500[count] + RH600[count]) / 2.0)
+        RH450.append((RH400[count] + RH500[count]) / 2.0)
+        RH350.append((RH300[count] + RH400[count]) / 2.0)
+        RH250.append((RH200[count] + RH300[count]) / 2.0)
+        count += 1
+
+    print('RH analyzed.')
+    return [RH1000,RH950,RH900,RH850,RH800,RH750,RH700,RH650,RH600,RH550,RH500,RH450,RH400,RH350,RH300,RH250,RH200]
+
+def analyzedetailwindV(source,JSON):
+    model = JSON['header']['model']
+    reftime = JSON['header']['refTime']
+    WV1000 = JSON['data']['wind_v-1000h']
+    WV950 = JSON['data']['wind_v-950h']
+    WV900 = JSON['data']['wind_v-900h']
+    WV850 = JSON['data']['wind_v-850h']
+    WV800 = JSON['data']['wind_v-800h']
+    WV700 = JSON['data']['wind_v-700h']
+    WV600 = JSON['data']['wind_v-600h']
+    WV500 = JSON['data']['wind_v-500h']
+    WV400 = JSON['data']['wind_v-400h']
+    WV300 = JSON['data']['wind_v-300h']
+    WV200 = JSON['data']['wind_v-200h']
+
+    WV750 = []
+    WV650 = []
+    WV550 = []
+    WV450 = []
+    WV350 = []
+    WV250 = []
+    count = 0
+    for i in WV800:
+        WV750.append((WV700[count] + WV800[count]) / 2.0)
+        WV650.append((WV600[count] + WV700[count]) / 2.0)
+        WV550.append((WV500[count] + WV600[count]) / 2.0)
+        WV450.append((WV400[count] + WV500[count]) / 2.0)
+        WV350.append((WV300[count] + WV400[count]) / 2.0)
+        WV250.append((WV200[count] + WV300[count]) / 2.0)
+        count += 1
+
+    print('WV analyzed.')
+    return [WV1000,WV950,WV900,WV850,WV800,WV750,WV700,WV650,WV600,WV550,WV500,WV450,WV400,WV350,WV300,WV250,WV200]
+
+def analyzedetailwindU(source,JSON):
+    model = JSON['header']['model']
+    reftime = JSON['header']['refTime']
+    WU1000 = JSON['data']['wind_u-1000h']
+    WU950 = JSON['data']['wind_u-950h']
+    WU900 = JSON['data']['wind_u-900h']
+    WU850 = JSON['data']['wind_u-850h']
+    WU800 = JSON['data']['wind_u-800h']
+    WU700 = JSON['data']['wind_u-700h']
+    WU600 = JSON['data']['wind_u-600h']
+    WU500 = JSON['data']['wind_u-500h']
+    WU400 = JSON['data']['wind_u-400h']
+    WU300 = JSON['data']['wind_u-300h']
+    WU200 = JSON['data']['wind_u-200h']
+
+    WU750 = []
+    WU650 = []
+    WU550 = []
+    WU450 = []
+    WU350 = []
+    WU250 = []
+    count = 0
+    for i in WU800:
+        WU750.append((WU700[count] + WU800[count])/2.0)
+        WU650.append((WU600[count] + WU700[count]) / 2.0)
+        WU550.append((WU500[count] + WU600[count]) / 2.0)
+        WU450.append((WU400[count] + WU500[count]) / 2.0)
+        WU350.append((WU300[count] + WU400[count]) / 2.0)
+        WU250.append((WU200[count] + WU300[count]) / 2.0)
+        count += 1
+
+    print('WU analyzed.')
+    return [WU1000,WU950,WU900,WU850,WU800,WU750,WU700,WU650,WU600,WU550,WU500,WU450,WU400,WU350,WU300,WU250,WU200]
+
 def dailygraph():
     global date
     global HI
@@ -88,8 +254,8 @@ def dailygraph():
     plt.xlabel("Date")
     plt.ylabel("Temperature(°C)")
     plt.title("Temprerature forecast in Toronto")
-    plt.savefig("/root/web/static/easyplot.png")
-    #plt.show()
+    #plt.savefig("/root/web/static/easyplot.png")
+    plt.show()
 
 def graph(source, list, values1, values2):
     timeseq = []
@@ -108,7 +274,7 @@ def graph(source, list, values1, values2):
     '''
     for i in range(0,len(list)):
         datelist.append(list[i])
-        datelist[i] = time.strftime('%d%H', time.localtime(list[i] / 1000.0 - 8 * 60 * 60))
+        datelist[i] = time.strftime('%d%H', time.localtime(list[i] / 1000.0 - 4 * 60 * 60))
         timeseq.append(i*3)
     print('DATELIST = ' + str(datelist))
     #dates = [datetime.datetime.strptime(elem, '%d %H') for elem in datelist]
@@ -144,9 +310,9 @@ def graph(source, list, values1, values2):
     ax1.xaxis.set_major_locator(MultipleLocator(12))
     if source == 'EC':
         plt.title('Toronto Weather Forecast for next 120 hours\n Model: ECMWF; Predict time from ' +
-                  time.strftime('20%y-%m-%d %H:00', time.localtime(list[0] / 1000.0 - 8 * 60 * 60)))
+                  time.strftime('20%y-%m-%d %H:00', time.localtime(list[0] / 1000.0 - 4 * 60 * 60)))
 
-    plt.savefig("/root/web/static/detailplot.png")
+    #plt.savefig("/root/web/static/detailplot.png")
     #plt.plot_date(dates, values)
     #plt.xticks(timeseq, datelist, size='small', rotation=30)
 
@@ -155,16 +321,98 @@ def getweather():
     global HI
     global LOW
     global source
-    iodata = getData(source, -79.399, 43.663)
-    analyze(source, iodata)
-    dailygraph()
+    #lon = -79.399
+    #lat = 43.663
+    lon = 121.25
+    lat = 31.45
+    iodata = getdetail(source, lon, lat)
+    hourspoint = iodata['data']['hours']
+
+    for i in range(0,len(hourspoint)):
+        hourspoint[i] = hourspoint[i] / 1000.0
+
+    dates = [time.strftime('%m%d %Hz', time.localtime(ts)) for ts in hourspoint]
+    newdates = []
+    ticks = []
+    count = 1
+    for i in dates:
+        if count % 2 ==0:
+            newdates.append(i)
+            ticks.append(count)
+        count += 1
+    print(dates)
+
+    print('DATA RECEIVED.')
+    Tdata = analyzedetailT('EC',iodata)
+    RHdata = analyzedetailRH('EC',iodata)
+    WVdata = analyzedetailwindV('EC', iodata)
+    WUdata = analyzedetailwindU('EC', iodata)
+
+    if lon < 0:
+        lon = -1 * lon
+        LON = 'W'
+    else:
+        LON = 'E'
+
+    if lat < 0:
+        lat = -1 * lat
+        LAT = 'S'
+    else:
+        LAT = 'N'
+
+
+    xmax = len(Tdata[0])
+    deltay = -50
+    x = np.arange(1, xmax+1, 1)
+    y = np.arange(1000, 150, deltay)
+    X, Y = np.meshgrid(x, y)
+
+    fig = plt.figure(figsize=(14,6),dpi=150)
+    fig.add_axes([0.1,0.1,0.8,0.78])
+    levels = [-50,-45,-40,-35,-30,-25,-20,-15,-10,-5,0,5,10,15,20,25,30,35]
+    CS = plt.contour(X, Y, Tdata, levels, colors = 'B')
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=100)
+    plt.clabel(CS, inline=1, fontsize=8)
+    plt.title('Temperature and Humidity Forecast @ Louis-He\n'+'Forecast Location:'+str(lon)+LON+', '+str(lat)+LAT+'\n'+'Model:'+iodata['header']['model']+' Init time: ' + iodata['header']['refTime'] + ' UTC', multialignment='left', fontsize = 10)
+    plt.gca().invert_yaxis()
+    plt.contourf(X, Y, RHdata, cmap=cm.PuBu, norm=norm)
+    cbar = plt.colorbar()
+    cbar.set_label('%')
+    cbar.set_ticks(np.linspace(0, 100, 10))
+    cbar.set_ticklabels(('0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100'))
+
+    #Q = plt.quiver(X, Y, WUdata, WVdata)
+    plt.barbs(X, Y, WUdata, WVdata, length=4,
+            sizes=dict(emptybarb=0, spacing=0.2, height=0.5), barb_increments=dict(half=2, full=4, flag=20),
+            linewidth=0.4, color='black')
+
+    plt.xticks(ticks, newdates, rotation=45)
+
+    plt.gca().set_xlim(1,count-1)
+    plt.gca().set_ylim(1000,200)
+    #plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',coordinates='figure')
+    '''
+    dfm = matplotlib.dates.DateFormatter('%m-%d %Hz')
+    locator = matplotlib.dates.HourLocator(byhour=[0,12])
+    plt.gca().xaxis.set_major_formatter(dfm)
+    plt.gca().xaxis.set_major_locator(locator)
+    plt.gca().set_xlim((dates[0],dates[len(dates)-1]))
+    '''
+
+    #fig.autofmt_xdate()
+
+    #plt.show()
+    #analyze(source, iodata)
+    #dailygraph()
+    plt.savefig('TS_'+str(lon)+str(lat)+'.png')
 
 date = []
 HI = []
 LOW = []
 source = 'EC'
 getweather()
-
+'''
 sched = BlockingScheduler()
 sched.add_job(getweather, 'interval', seconds = 3 * 60 * 60)
 sched.start()
+'''
