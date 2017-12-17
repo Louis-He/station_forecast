f877733b1ba0af91be0cc9c0efb7cd9449d35e53
Louis-He <1726110778@qq.com>
2017-12-17 00:37:08 -0500

    update

--- website/webfunc.py
+++ website/webfunc.py
@@ -1,4 +1,5 @@
 import web
+import os
 urls = (
     '/index', 'index',
     '/login', 'login',
@@ -89,9 +90,40 @@ class missionlist:
 
 class product:
     def GET(self):
+        path = 'static/images/'
+        files = os.listdir(path)
+        piclist = [];
+        colcount = 0
+        rowcount = 0
+        result = ''
+        for file in files:
+            if colcount == 0:
+                result += '<div class="row mt">'
+
+            result += '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc"><div class="project-wrapper"><div class="project"><div class="photo-wrapper"><div class="photo">'
+            result += '<a class="fancybox" href="static/images/'+file+'"><img class="img-responsive" src="static/images/'+file+'" alt=""></a>'
+            result += '<div class="row mt"><div class="col-lg-12">'
+            file = file[file.find('_'):]
+            if file.find('E') != -1:
+                lon = file[0:file.find('E') + 1]
+            else:
+                lon = file[0:file.find('W') + 1]
+
+            if file.find('N') != -1:
+                lat = file[0:file.find('N') + 1]
+            else:
+                lat = file[0:file.find('S') + 1]
+
+            result += '<p>时序图（坐标：'+ lon + ',' + lat +'）</p>'
+            result += '</div></div></div><div class="overlay"></div></div></div></div></div><!-- col-lg-4 -->'
+
+            if colcount == 2:
+                result += '</div><!-- /row -->'
+            colcount += 1
+
         if iscookie()==True:
-            print(open(r'product.html', 'r').read())
-            return open(r'product.html', 'r').read()
+            #print(open(r'product.html', 'r').read())
+            return open(r'producthead.html', 'r').read() + result + open(r'productbuttom.html', 'r').read()
         else:
             return open(r'login.html', 'r').read()
 
