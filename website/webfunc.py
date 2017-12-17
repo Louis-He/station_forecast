5dd4e283c007fe837fb9e7e3724f320aa1ad435c
Louis-He <1726110778@qq.com>
2017-12-17 12:46:14 -0500

    update

--- website/webfunc.py
+++ website/webfunc.py
@@ -172,6 +172,8 @@ class addmission:
             read = True
             f = open('waitlistmission.sh', 'a+')
             f.write('python3 main.py --lon ' + lon + ' --lat '+ lat +' --source ' + source + '\n')
+            print('!![LON]!!'+lon)
+            print('!![LAT]!!'+lat)
             print('!![PRCOESS]!!')
             f.close()
 
